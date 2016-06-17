1bef10b2afd5026c75442fb55ffa00b177d52346
johardi <josef.hardi@stanford.edu>
2016-06-17 11:41:24 -0700

    Add new types of index fields

--- src/main/java/org/protege/editor/search/lucene/IndexField.java
+++ src/main/java/org/protege/editor/search/lucene/IndexField.java
@@ -10,15 +10,27 @@ public final class IndexField {
 
     public static final String ENTITY_IRI = "entityIri";
 
-    public static final String ENTITY_TYPE = "type";
-
     public static final String DISPLAY_NAME = "displayName";
 
     public static final String PHONETIC_NAME = "phoneticName";
 
-    public static final String ANNOTATION_IRI = "annotator";
+    public static final String ENTITY_TYPE = "entityType";
+
+    public static final String OBJECT_PROPERTY_IRI = "objectPropertyIri";
+
+    public static final String OBJECT_PROPERTY_DISPLAY_NAME = "objectPropertyDisplayName";
+
+    public static final String DATA_PROPERTY_IRI = "dataPropertyIri";
+
+    public static final String DATA_PROPERTY_DISPLAY_NAME = "dataPropertyDisplayName";
+
+    public static final String ANNOTATION_IRI = "annotationIri";
 
     public static final String ANNOTATION_DISPLAY_NAME = "annotationDisplayName";
 
     public static final String ANNOTATION_TEXT = "annotationText";
+
+    public static final String FILLER_IRI = "fillerIri";
+
+    public static final String FILLER_DISPLAY_NAME = "fillerDisplayName";
 }
