412394a37433216c85024bdf9ed5eed6a65d74a4
johardi <josef.hardi@stanford.edu>
2016-06-21 11:22:18 -0700

    Add QueryType enumeration class

---
+++ src/main/java/org/protege/editor/search/nci/QueryType.java
@@ -0,0 +1,105 @@
+package org.protege.editor.search.nci;
+
+import org.semanticweb.owlapi.model.OWLAnnotationProperty;
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLObject;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.util.CollectionFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class QueryType {
+
+    private final String name;
+    private final String displayName;
+    private final boolean valueType;
+    private final boolean nonValueType;
+    private final boolean booleanType;
+
+    private QueryType(String name, String displayName, boolean valueType, boolean nonValueType, boolean booleanType) {
+        this.name = name;
+        this.displayName = displayName;
+        this.valueType = valueType;
+        this.nonValueType = valueType;
+        this.booleanType = booleanType;
+    }
+
+    private static QueryType getInstance(String name, String displayName, boolean valueType, boolean nonValueType,
+            boolean booleanType) {
+        return new QueryType(name, displayName, valueType, nonValueType, booleanType);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public boolean isValueType() {
+        return valueType;
+    }
+
+    public boolean isNonValueType() {
+        return nonValueType;
+    }
+
+    public boolean isBooleanType() {
+        return booleanType;
+    }
+
+    public Set<QueryType> getTypesForOWLObject(OWLObject owlObject) {
+        Set<QueryType> types = new HashSet<>();
+        if (owlObject instanceof OWLDataProperty) {
+            types.addAll(ValueQueryTypes);
+            types.addAll(NonValueQueryTypes);
+        }
+        else if (owlObject instanceof OWLObjectProperty) {
+            types.addAll(NonValueQueryTypes);
+        }
+        else if (owlObject instanceof OWLAnnotationProperty) {
+            types.addAll(ValueQueryTypes);
+            types.addAll(NonValueQueryTypes);
+        }
+        return types; 
+    }
+
+    //@formatter:off
+    public static final QueryType CONTAINS = getInstance("ContainsQuery", "contains", true, false, false);
+    public static final QueryType STARTS_WITH = getInstance("StartsWithQuery", "starts with", true, false, false);
+    public static final QueryType ENDS_WITH = getInstance("EndsWithQuery", "ends with", true, false, false);
+    public static final QueryType EXACT_MATCH = getInstance("ExactMatchQuery", "exact match", true, false, false);
+    public static final QueryType PROPERTY_VALUE_ABSENT = getInstance("PropertyValueAbsentQuery", "property value absent", false, true, false);
+    public static final QueryType PROPERTY_VALUE_PRESENT = getInstance("PropertyValuePresentQuery", "property value present", false, true, false);
+    public static final QueryType PROPERTY_RESTRICTION_ABSENT = getInstance("PropertyRestrictionAbsentQuery", "property restriction absent", false, true, false);
+    public static final QueryType PROPERTY_RESTRICTION_PRESENT = getInstance("PropertyRestrictionPresentQuery", "property restriction present", false, true, false);
+    public static final QueryType IS = getInstance("IsQuery", "is", false, false, true);
+
+    //@formatter:off
+    public static final Set<QueryType> QUERY_TYPES = CollectionFactory.createSet(
+            CONTAINS,
+            STARTS_WITH,
+            ENDS_WITH,
+            EXACT_MATCH,
+            PROPERTY_VALUE_ABSENT,
+            PROPERTY_VALUE_PRESENT,
+            PROPERTY_RESTRICTION_ABSENT,
+            PROPERTY_RESTRICTION_PRESENT,
+            IS);
+
+    public static final Set<QueryType> ValueQueryTypes = CollectionFactory.createSet(
+            CONTAINS,
+            STARTS_WITH,
+            ENDS_WITH,
+            EXACT_MATCH);
+
+    public static final Set<QueryType> NonValueQueryTypes = CollectionFactory.createSet(
+            PROPERTY_VALUE_ABSENT,
+            PROPERTY_VALUE_PRESENT,
+            PROPERTY_RESTRICTION_ABSENT,
+            PROPERTY_RESTRICTION_PRESENT);
+
+    public static final Set<QueryType> BooleanQueryTypes = CollectionFactory.createSet(IS);
+}
