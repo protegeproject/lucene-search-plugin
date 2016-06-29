2682d05eef3a51e36d851345a928ee734bffa323
Rafael Goncalves <goncalvjr@gmail.com>
2016-06-29 15:51:10 -0700

    Added Lucene events enum and listener interface

---
+++ src/main/java/org/protege/editor/search/ui/LuceneListener.java
@@ -0,0 +1,12 @@
+package org.protege.editor.search.ui;
+
+/**
+ * @author Rafael Gonçalves <br>
+ * Center for Biomedical Informatics Research <br>
+ * Stanford University
+ */
+public interface LuceneListener {
+
+    void statusChanged(LuceneEvent event);
+
+}
